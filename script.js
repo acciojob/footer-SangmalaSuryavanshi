19422a0ed3a917f8b25116a8d675bd8b69b4404e
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-09-08 22:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-describe('Footer Test', () => {
-  it('should display footer elements correctly', () => {
-    cy.visit('http://localhost:3000/');
-    
-    // Check if the .footer-container has a display of flex
-    cy.get('.footer-container').should('have.css', 'display', 'flex');
-
-    // Check for contact number presence
-    cy.get('.footer-container').contains('8879887262');
-
-    // Check for presence of text in the footer sections
-    cy.get('.footer-container').contains('Home');
-    cy.get('.footer-container').contains('About');
-    cy.get('.footer-container').contains('Links');
-    cy.get('.footer-container').contains('Services');
-    cy.get('.footer-container').contains('Contact');
-  });
+cy.visit('http://localhost:3000/');
+cy.get('body').then(($body) => {
+  // Check if the element exists
+  if ($body.find('.link-boxes').length > 0) {
+    // Element exists
+    cy.get('.link-boxes').should('be.visible');
+  } else {
+    // Element does not exist
+    throw new Error('Element .link-boxes does not exist');
+  }
 });
